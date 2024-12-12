4b83c6e831421f773d1ea10f1597be04fca69606
Hideto Manjo <enjinit@gmail.com>
2024-12-12 20:34:16 +0900

    modified:   app.py

--- simulation/app.py
+++ simulation/app.py
@@ -71,7 +71,10 @@ def get_all_jobs():
     all_jobs = {}
     for job_key in redis.keys(f"{REDIS_JOB_PREFIX}*:meta"):
         job_id = job_key.replace(REDIS_JOB_PREFIX, "").replace(":meta", "")
-        job_data = json.loads(redis.get(job_key).decode('utf-8'))
+        job_data = redis.get(job_key)
+        if isinstance(job_data, bytes):
+            job_data = job_data.decode('utf-8')
+        job_data = json.loads(job_data)
         all_jobs[job_id] = job_data
     return all_jobs
 
