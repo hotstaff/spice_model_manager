86dcd24902a06fada38bde95facf5f34cb05dd39
Hideto Manjo <enjinit@gmail.com>
2024-12-15 11:12:17 +0900

    modified:   app.py 	deleted:    simulation_view.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, redirect, url_for
 from models.db_model import init_db, migrate_db
 from views import model_views  # views.pyからmodel_viewsをインポート
-from simulation_view import simulation
+from simulation_view import simu_views
 
 app = Flask(__name__)
 
@@ -14,6 +14,7 @@ migrate_db()
 
 # APIエンドポイントを設定
 app.register_blueprint(model_views)
+app.register_blueprint(simu_views)
 
 @app.route('/')
 def home():
