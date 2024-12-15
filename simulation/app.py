d28be13800651195f84d3b93142c8051b4686aa0
Hideto Manjo <enjinit@gmail.com>
2024-12-15 10:36:54 +0900

    modified:   app.py

--- simulation/app.py
+++ simulation/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY', os.urandom(24))
 
 # APIエンドポイントを設定
-app.register_blueprint(simulation)
+app.register_blueprint(simu)
 
 if __name__ == "__main__":
     app.run(debug=False, port=5000)
