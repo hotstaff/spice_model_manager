83f5ebd63c8fd481715bdde1d4f4775e8e63707b
Hideto Manjo <enjinit@gmail.com>
2025-01-01 21:05:34 +0900

    modified:   simulation/jfet.py 	modified:   templates/build.html

--- simulation/jfet.py
+++ simulation/jfet.py
@@ -6,7 +6,6 @@ from PyLTSpice import SpiceEditor, RawRead  # PyLTSpiceライブラリの必要�
 
 from bokeh.plotting import figure
 from bokeh.embed import json_item
-import json
 
 class JFET_SimulationBase:
 
@@ -223,7 +222,7 @@ class JFET_IV_Characteristic(JFET_SimulationBase):
         p.legend.location = "top_left"
 
         # プロットデータをJSON形式でエクスポート
-        plot_json = json.dumps(json_item(p))
+        plot_json = json_item(p)
 
         # JSONデータを返す
         return plot_json
