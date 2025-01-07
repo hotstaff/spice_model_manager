fcaa57449d0f9bd0ac18ac2d6850d00583eaeaa1
Hideto Manjo <enjinit@gmail.com>
2025-01-07 21:07:19 +0900

    modified:   simulation_views.py

--- simulation_views.py
+++ simulation_views.py
@@ -361,7 +361,7 @@ def upload_file():
                 df = pd.read_csv(file_bytes)
 
                 # データフレームをHTMLテーブルに変換
-                table_html = df.to_html(classes='table table-auto')
+                table_html = df.to_html(classes='table table-auto border-separate')
             except Exception as e:
                 return f"Error processing file: {e}"
 
