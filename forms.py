eef06e3e56de5875f83eab7650ce7661fd7b33a3
Hideto Manjo <enjinit@gmail.com>
2025-01-21 16:03:19 +0900

    modified:   forms.py 	modified:   simulation_views.py

--- forms.py
+++ forms.py
@@ -2,8 +2,8 @@ import re
 import logging
 
 from flask import flash, redirect, url_for
-from wtforms import Form, StringField, FileField
-from wtforms.validators import DataRequired, Length, Regexp, Optional, InputRequired
+from wtforms import Form, StringField
+from wtforms.validators import DataRequired, Length, Regexp, Optional
 
 from client.spice_model_parser import SpiceModelParser
 
