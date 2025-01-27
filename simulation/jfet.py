83e91f0232494975e6be9b13c7f4b58c24eea947
Hideto Manjo <enjinit@gmail.com>
2025-01-27 19:46:12 +0900

    modified:   simulation/jfet.py

--- simulation/jfet.py
+++ simulation/jfet.py
@@ -10,10 +10,17 @@ import json
 
 import itertools
 
-# 色リスト
 color_map = [
-    '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', 
-    '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'
+    '#1f77b4',  # 青
+    '#ff7f0e',  # 橙
+    '#2ca02c',  # 緑
+    '#d62728',  # 赤
+    '#9467bd',  # 紫
+    '#8c564b',  # 茶
+    '#e377c2',  # ピンク
+    '#7f7f7f',  # 灰
+    '#bcbd22',  # 黄緑
+    '#17becf'   # シアン
 ]
 
 class JFET_SimulationBase:
