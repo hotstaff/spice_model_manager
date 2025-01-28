6b22999178a50cf13d0f389b023047288cc61226
Hideto Manjo <enjinit@gmail.com>
2025-01-28 21:52:29 +0900

    modified:   simulation_views.py

--- simulation_views.py
+++ simulation_views.py
@@ -166,7 +166,7 @@ def run_simulate_now_api(output_format):
 
 
     # シミュレーション設定
-    configs = model.show_config()
+    configs = model.show_default_config()
 
     for key, default_value in configs.items():
         # request.form.get()で、フォームから取得した値があればそれを使い、なければdefault_valueを使う
