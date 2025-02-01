09c20df42e8cd78d6b79efeff884d2b014069928
Hideto Manjo <enjinit@gmail.com>
2025-02-01 20:06:45 +0900

    modified:   simulation_views.py

--- simulation_views.py
+++ simulation_views.py
@@ -163,12 +163,12 @@ def run_simulate_now_api(output_format):
 
     # シミュレーション設定
     configs = model.show_default_config()
-    
+
     print(configs)
 
     for key, default_value in configs.items():
 
-        if key is 'LIMITS':
+        if key == 'LIMITS':
             continue
 
         # request.form.get()で、フォームから取得した値があればそれを使い、なければdefault_valueを使う
